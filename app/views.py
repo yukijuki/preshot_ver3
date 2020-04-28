a227cf65a7dce64a88b5fefa342c356789ffb71c
ronasuti <rnn.mpi@gmail.com>
2020-04-28 14:19:41 +0900

    Accidentally didn't pull out the items from paginate, here's a fix
    Signed-off-by: ronasuti <rnn.mpi@gmail.com>

--- app/views.py
+++ app/views.py
@@ -535,7 +535,7 @@ def mentor_home():
 
     page: int = request.args.get('page', 1, type=int)
 
-    posts = Post.query.paginate(page, 10, False)
+    posts = Post.query.paginate(page, 10, False).items
     next_url = url_for('index', page=posts.next_num) if posts.has_next else None
     prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
     response = []
