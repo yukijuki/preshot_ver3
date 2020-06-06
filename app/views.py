d70bb350e3688ef3bc648673fa845cb6a73a35d1
YukiKawano <yukijuki@YukiKawanos-MacBook-Pro.local>
2020-06-06 17:39:47 +0900

    yui

--- app/views.py
+++ app/views.py
@@ -432,7 +432,7 @@ def chat(rid):
         "day": schedule.day,
         "place": schedule.place,
         "rid": reservation.rid,
-        "name": mentor.name+"さん",
+        "name": mentor.name,
         "filename": 'static/img-get/' + mentor.filename
     }
 
