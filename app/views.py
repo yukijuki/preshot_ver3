17261e734547e99fc57c18798851e0b205acd4fd
hayatespinner <white8810wnew@gmail.com>
2020-06-12 11:20:49 +0900

    icon

--- app/views.py
+++ app/views.py
@@ -106,6 +106,7 @@ class Chat(db.Model):
     message = db.Column(db.UnicodeText(), nullable=False)
     created_at = db.Column(db.DateTime(), nullable=False)
 
+# db.create_all()
 
 
 # ----------------------------------------------------------------
