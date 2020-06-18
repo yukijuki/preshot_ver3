0a66e129ac8cf51823fa34d5a48eee8f1f828176
YukiKawano <yukijuki@YukiKawanos-MacBook-Pro.local>
2020-06-19 00:20:09 +0900

    yuki

--- app/views.py
+++ app/views.py
@@ -21,7 +21,8 @@ POSTS_PER_PAGE = 10
 # This now requires Postgresql, feel free to use a GUI app like Postgres.app (I'm using that).
 # Don't worry, Postgresql doesn't really do anything when you aren't querying it,
 # So feel free to leave it on.
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://preshot:wepreshot@localhost:5432/preshot"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://preshot:wepreshot@localhost:5432/preshot"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = hashlib.sha256(b"wepreshot").hexdigest()
 app.config["UPLOAD_FOLDER"] = PHYSICAL_ROOT + UPLOAD_FOLDER
@@ -148,9 +149,9 @@ def index():
     post = Post.query.count()
 
     data = {
-            "student": student-1,
-            "mentor": mentor-1,
-            "reservation": reservation-1,
+            "student": student,
+            "mentor": mentor,
+            "reservation": reservation,
             "post": post
             }
 
@@ -217,7 +218,7 @@ def register():
             db.session.add(newuser)
             db.session.commit()
             flash("アカウントが作成されました")
-            return redirect(url_for('post'))
+            return redirect(url_for('mypost'))
 
         else:
             if student.password == hashlib.sha256((data["password"]+data["email"]).encode('utf-8')).hexdigest():
