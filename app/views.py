29011caaa2c43879bc2edc6303c1445a6068b929
YukiKawano <yukijuki@YukiKawanos-MacBook-Pro.local>
2020-07-09 01:37:12 +0900

    yuki

--- app/views.py
+++ app/views.py
@@ -450,7 +450,7 @@ def reservation(sid):
 
     #-------Hey roman here is what I need you to do! 1 and 2----------
 
-    #1. I need this data to be get the data with the sid and pid condition but pid missing in Reservation schema.
+    #1. I need this data to be get the data with the sid and pid and uid condition but pid missing in Reservation schema.
     reservation = Reservation.query.filter_by(schedule_id=sid).first()
     #reservation = Reservation.query.filter_by(schedule_id=sid).filter_by(pid=pid).first()
 
