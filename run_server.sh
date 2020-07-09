73c5d5a0d8a3dfd0a31ae43a06ace34fc554f5df
Preshot <preshot.dev@gmail.com>
2020-07-09 02:31:34 +0000

    changed run_server.sh to not use cached files, prevents errors

--- run_server.sh
+++ run_server.sh
@@ -5,6 +5,6 @@ pkill -f run.py
 rm -rf venv
 python3 -m venv venv
 source venv/bin/activate
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt --no-cache-dir
 #run server
 nohup python3 run.py &
